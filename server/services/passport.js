ad1da508011d33bc0843c1ca73a6d0bbef454310
coderishu <ishu.mishra97@gmail.com>
2020-01-19 17:09:20 +0530

    commiting 19th jan 2019

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,8 +2,17 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-
 const User = mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+  User.findById(id).then(user=>{
+    done(null,user);
+  })
+})
 
 passport.use(
     new GoogleStrategy(
@@ -13,7 +22,15 @@ passport.use(
         callbackURL: '/auth/google/callback'
       },
       (token,refreshtoken,profile,done) => {
-        new User({googleId:profile.id}).save();
+        User.findOne({googleId:profile.id})
+        .then((existinguser)=>{
+          if(existinguser){
+      done(null,existinguser);
+          }
+          else{
+            new User({googleId:profile.id}).save().then(user=>done(null,user));
+          }
+        })
       }
     )
   );
