fbe2b753ae609eae63bb3326beea135c647ea0a4
coderishu <ishu.mishra97@gmail.com>
2020-02-09 06:50:07 +0530

    commiting

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component{
+  renderContent(){
+    switch(this.props.auth){
+      case null:
+      return 'Still deciding';
+      case false:
+      return 'im logged out';
+      default:
+      return 'im logged in'
+    }
+  }
     render(){
         return(
             <nav>
             <div className="nav-wrapper">
               <a href="#" className="left brand-logo">Emaily</a>
               <ul id="nav-mobile" className="right">
-                <li><a href="sass.html">Login With Google</a></li>
+                <li>
+                  {this.renderContent()}
+                  </li>
               </ul>
             </div>
           </nav>
@@ -15,4 +28,7 @@ class Header extends Component{
         )
     }
 }
-export default Header;
+function mapStateToProps({auth}){
+  return{auth}
+}
+export default connect(mapStateToProps)(Header);
