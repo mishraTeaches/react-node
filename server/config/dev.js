a2fb843ca509f3d2336e2b3c1f491a54d5ce7efb
coderishu <ishu.mishra97@gmail.com>
2020-02-28 21:05:25 +0530

    commiting

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientID:'335681727834-bprbveijqc6de20q7uhk2ebdl5217d16.apps.googleusercontent.com',
+    googleClientSecret:'oAQNUeTBCgISh6vN5khLU_h-',
+    mongoURI:'mongodb://ishu0005:coding123@ds263368.mlab.com:63368/emaily',
+    cookieKey:'ahsgtsbshyiokdndujtddpdpdjfff'
+}
