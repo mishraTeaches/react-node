214a049c6b08641c60f1ba64e8635b85e68f23c0
coderishu <ishu.mishra97@gmail.com>
2020-03-14 20:22:54 +0530

    routing backend o frontend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -8,6 +9,7 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI,{useNewUrlParser:true});
 const app = express();
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge:30*24*60*60*1000,
@@ -19,6 +21,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV==='production'){
+    // express will serve Production assets like our main.js or main.css file
+app.use(express.static('client/build'));
+
+// express will serve up the index.html file,if it doesnt recognize the route
+const path=require('path');
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+});
+}
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
