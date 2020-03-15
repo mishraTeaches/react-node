bf387e5f8ab78a3ae49bb0d97a2d3a79d0419b1c
coderishu <ishu.mishra97@gmail.com>
2020-03-15 08:29:16 +0530

    comm

---
+++ server/config/dev.js
@@ -0,0 +1,8 @@
+module.exports={
+    googleClientID:'335681727834-bprbveijqc6de20q7uhk2ebdl5217d16.apps.googleusercontent.com',
+    googleClientSecret:'oAQNUeTBCgISh6vN5khLU_h-',
+    mongoURI:'mongodb://ishu0005:coding123@ds263368.mlab.com:63368/emaily',
+    cookieKey:'ahsgtsbshyiokdndujtddpdpdjfff',
+    stripePublishableKey:'pk_test_X2eIuNdAzGPAidIfKEhSjfoQ00qCEef7xi',
+    stripeSecretKey:'sk_test_v8PBRD5tMVCqYeRzXApp50Fy00HjKNkf8j'
+}
